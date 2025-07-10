1ad27a2de5372f399295ed02449cf4ee49f1b462
AdityaAnoop3 <adityaanoop@live.in>
2025-07-11 00:52:18 +0400

    Changes to API URL

--- studyflow-frontend/src/services/api.ts
+++ studyflow-frontend/src/services/api.ts
@@ -1,7 +1,6 @@
 import axios from 'axios'
 
-// Temporarily hardcode to test
-const API_URL = 'http://localhost:5001/api'
+const API_URL = import.meta.env.VITE_API_URL
 
 // Debug log
 console.log('API_URL:', API_URL)
